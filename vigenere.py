47046c642c93ea733f8807c4794df8a95ce2cdf8
VictorMauroy <mauroy.victor@gmail.com>
2023-06-05 18:21:36 +0200

    Feat: :construction: add decrypt function for length 4 lists

--- vigenere.py
+++ vigenere.py
@@ -1,6 +1,15 @@
 from random import randint
 
 MY_MESSAGE = "It's time to create an original message !"
+MY_VERY_LOOOOOOOOONG_MESSAGE = """It's time to create an original message !
+I don't know what to say but I guess that it's important to write things to get a better result.
+Don't you think ? Have a nice day. Maybe I should have used a lorem ipsum... crap. 
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt 
+in culpa qui officia deserunt mollit anim id est laborum."""
+
 # Create a random size list with random int values
 my_key = [randint(0, 9) for _ in range(4)]
 
@@ -24,7 +33,33 @@ def decipher_message(message_to_decipher:str, key:list) -> str :
         if(key_index == len(key)) : key_index = 0
     return deciphered_message
 
-my_enciphered_message = encipher_message(MY_MESSAGE, my_key)
+def decrypt_message(message_to_decrypt:str) -> str :
+    """Decrypt a given message by finding its key (random list) and return the result"""
+    decrypted_key = []
+    for i in range(4) :
+        character_frequency_dict = {}
+        for character in range(i, len(message_to_decrypt), 4) :
+            try :
+                character_frequency_dict[character] += 1
+            except :
+                character_frequency_dict[character] = 1
+
+        max_count = 0
+        most_frequent_char = ""
+        for character, value in character_frequency_dict.items() :
+            if value > max_count :
+                max_count = value
+                most_frequent_char = character
+
+        space_char_difference = ord(most_frequent_char) - ord(" ")
+        decrypted_key.append(space_char_difference)
+
+    decipher_message(message_to_decrypt, decrypted_key)
+
+"""my_enciphered_message = encipher_message(MY_MESSAGE, my_key)
 print(my_enciphered_message)
 my_deciphered_message = decipher_message(my_enciphered_message, my_key)
-print(my_deciphered_message)
+print(my_deciphered_message)"""
+
+long_crypted_message = encipher_message(MY_VERY_LOOOOOOOOONG_MESSAGE, my_key)
+print(decrypt_message(long_crypted_message))
